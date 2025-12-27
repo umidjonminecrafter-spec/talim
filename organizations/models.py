7d82dd0a0c2bbd74f44d0426dda0b175b7dcee48
UzbekDev2024 <157269376+UzbekDev2024@users.noreply.github.com>
2025-12-27 14:55:45 +0500

    yangilik

--- organizations/models.py
+++ organizations/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from core.validators import uz_phone_validator
 import uuid
+
+# Yangilangan organization model
 class Organizations(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     STATUS = (
